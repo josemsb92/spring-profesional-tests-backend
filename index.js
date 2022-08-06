d42434f4985d324c059200490dad194c4bacdd66
josemsb92 <josemsb92@gmail.com>
2022-08-07 01:24:13 +0200

    cors added

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
+const cors = require("cors");
 const express = require("express");
 const app = express();
+app.use(cors());
 const { sequelize } = require("./models");
 const port = process.env.PORT || 3000;
 app.use("/", require("./routes"));
